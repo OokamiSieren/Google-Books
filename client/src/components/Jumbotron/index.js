2a2aef5c63ddcc43d7e35d6da21424051bf20f4d
Diana <d_quinlan061006@yahoo.com>
2019-07-12 22:26:36 -0400

    added Jumbotron component, needs styling

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./style.css";
+function Jumbotron() {
+    return (
+      <div class="jumbotron jumbotron-fluid" id="jumbo">
+        <div class="container">
+          <h1 class="display-4" className="text-align-center">
+             Google Books! 
+          </h1>
+        </div>
+      </div>
+    );
+  }
+  export default Jumbotron;
