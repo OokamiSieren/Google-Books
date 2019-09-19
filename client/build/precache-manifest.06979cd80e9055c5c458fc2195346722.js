b7de9eb8b0f83c33ea75129342c35447411b1eb1
Diana <d_quinlan061006@yahoo.com>
2019-09-19 00:12:51 -0400

    added a .catch to get books

---
+++ client/build/precache-manifest.06979cd80e9055c5c458fc2195346722.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da451cae88eaf62d270b53bfa6c514c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdff12d074dfdcbe7d76",
+    "url": "/static/css/main.4a3305d5.chunk.css"
+  },
+  {
+    "revision": "5874619e6794b8a49dff",
+    "url": "/static/js/2.2dabd79c.chunk.js"
+  },
+  {
+    "revision": "cdff12d074dfdcbe7d76",
+    "url": "/static/js/main.efabd095.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8827a7df7ed37344026928db07ef945d",
+    "url": "/static/media/book.8827a7df.jpg"
+  },
+  {
+    "revision": "362e382fdd0feef3b5ff47ca50854259",
+    "url": "/static/media/books.362e382f.jpg"
+  },
+  {
+    "revision": "68ad9411a9a6c7a11f7eae2fc713b823",
+    "url": "/static/media/ricepaper_v3.68ad9411.png"
+  }
+]);
